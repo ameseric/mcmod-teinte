ed3566b017c198f47738ec8fe48f235d771de8cd
Eric Ames <amesen07@gmail.com>
2019-04-25 18:43:29 -0700

    Finalized Region GUi placement, fade-in/out time, etc. Just need to tweak the values for aesthetic purposes, going to check other game title fades. Region Logic is next.

--- java/witherwar/util/TeinteGUI.java
+++ java/witherwar/util/TeinteGUI.java
@@ -4,6 +4,7 @@ package witherwar.util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,7 +13,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 
 /**
- * Credit to EMAX2000, whose code I used below: https://emxtutorials.wordpress.com/simple-in-game-gui-overlay/ 
+ * Credit to EMAX2000, whose code started this class: https://emxtutorials.wordpress.com/simple-in-game-gui-overlay/ 
  */
 
 
@@ -42,23 +43,60 @@ public class TeinteGUI{
 	
 	@SideOnly( Side.CLIENT)
 	public class RegionOverlay extends Gui{
-		String text = "SAMPLE";
+		final int MAX_ALPHA = 255;
+		final float SCALE = 1.6F;
+		final float ISCALE = 1/SCALE;
+		final int DELAY = 60;
+		
+		String text = "Plains of Reprieve";
 		int tick = 0;
+		int delayCounter = 0;
+		boolean asc = true;
 		
 		public void draw( Minecraft mc){
 	        ScaledResolution scaled = new ScaledResolution( mc);
 	        int width = scaled.getScaledWidth();
 	        int height = scaled.getScaledHeight();
 	        
-	        int timer = tick/20;
-	        String textNow = text.substring( 0 ,timer);
-	        if( timer == text.length()) {
-	        	tick = 0;
-	        }
-	 
-	        drawString( mc.fontRenderer ,textNow ,width / 20 ,height / 10 ,Integer.parseInt("FFAA00", 16));
-	        tick++;
+	        int alpha = getFadeAlpha();
+	        
+	        GlStateManager.scale( SCALE ,SCALE ,SCALE);
+	        //drawCenteredString( mc.fontRenderer ,text ,Math.round( width/(2*SCALE)) ,Math.round( height/(SCALE*2) - ((DEFAULT_TEXT_PIXEL_HEIGHT/2) * SCALE)) 
+	        //		,Integer.parseInt("EEEEEE" ,16) | (alpha << 24));
+	        
+	        mc.fontRenderer.drawString( text ,width/(24*SCALE) ,height/(SCALE*14) ,Integer.parseInt("EEEEEE" ,16) | (alpha << 24) ,false);
+	        GlStateManager.scale( ISCALE ,ISCALE ,ISCALE);
+	        
 		}
+		
+		private int getFadeAlpha() {
+			
+			int alpha = Math.min( (int) Math.floor( (1.0F/16.0F) * Math.pow(tick ,2)) + 4 ,MAX_ALPHA);
+			System.out.println( alpha);
+			
+			if (asc) {
+				if( alpha == MAX_ALPHA) {
+					++delayCounter;
+					if( delayCounter == DELAY) {
+						asc = false;
+						delayCounter = 0;
+					}
+				}
+				++tick;
+			}else {
+				if( alpha == 4) {
+					asc = true;
+				}else {
+					--tick;
+				}
+			}
+			
+						
+			return alpha;
+		}
+		
+		
+
 	}
 	
 	
