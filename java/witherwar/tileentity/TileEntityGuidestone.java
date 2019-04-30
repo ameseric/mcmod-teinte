8005baca22fd5e45e1340d5932e9c1d2be61630c
Eric Ames <amesen07@gmail.com>
2019-04-29 17:57:38 -0700

    A minimally-tested working commit. Had to scrap using sign code, now using a custom GuiScreen. It runs, but lacks features. Also need a new server -> client message for displaying the current saved name of Guidestones.

--- java/witherwar/tileentity/TileEntityGuidestone.java
+++ java/witherwar/tileentity/TileEntityGuidestone.java
@@ -1,9 +1,21 @@
 package witherwar.tileentity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.ChunkPos;
+import witherwar.WitherWar;
+
 
 public class TileEntityGuidestone extends TileEntity{
+	public String regionName = "";
+	public List<ChunkPos> map = new ArrayList<ChunkPos>();
+	
+
+	public TileEntityGuidestone() {}
+	
 
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
     	super.writeToNBT(compound);
@@ -15,8 +27,31 @@ public class TileEntityGuidestone extends TileEntity{
     public void readFromNBT(NBTTagCompound compound) {
     	super.readFromNBT(compound);
     }
+    
+    
+    //on the tile entity being loaded, access and modify the WitherWar Regional dictionary
+    //method goes here
 
 
+	public void findRegionChunks() {
+		System.out.println( "Finding region chunks...");
+		this.map.add( new ChunkPos( this.pos.getX()%16 ,this.pos.getZ()%16));
+	}
+	
+	public boolean hasRegionChunks() {
+		System.out.println( "Checking region chunks...");
+		return ( this.map.size() > 0 );
+	}
+	
+	public void setRegionName( String regionName) {
+		this.regionName = regionName;
+	}
+	
+	
+	public void update( String regionName) {
+		this.setRegionName(regionName);
+		WitherWar.instance.setRegionalMap( this);
+	}
 	
 	
 }
