b46047e77243164d81240d54a37a87c983b43547
Eric Ames <amesen07@gmail.com>
2019-05-10 13:47:28 -0700

    Basic custom model working. Using vanilla texture. Culling is appropriate to shape. All that's left is to modify the bounding box (since it's above 16 tall) or decide on a different model size to use. This is mostly a question of aesthetics now, and will probably be saved for later.

--- java/witherwar/block/BlockGuidestone.java
+++ java/witherwar/block/BlockGuidestone.java
@@ -6,11 +6,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import witherwar.WitherWar;
 
 
@@ -38,6 +41,17 @@ public class BlockGuidestone extends Block{
     public EnumBlockRenderType getRenderType( IBlockState state){
         return EnumBlockRenderType.MODEL;
     }
+	
+	@Override
+	public boolean isFullCube( IBlockState state) {
+		return false;
+	}
+	
+	@Override
+	public boolean isOpaqueCube( IBlockState state) {
+		return false;
+	}
+
 	
 	@Override
     public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
